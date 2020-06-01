a4eff269ca8b2d6ede973124292a992b7be1643c
Anatoliy Korovin <antkorwin@gmail.com>
2020-06-02 08:16:56 +1000

    simple application

--- src/test/java/com/antkorwin/githubactions/HelloControllerTest.java
+++ src/test/java/com/antkorwin/githubactions/HelloControllerTest.java
@@ -3,9 +3,11 @@ package com.antkorwin.githubactions;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
+@AutoConfigureWebTestClient
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class HelloControllerTest {
 
